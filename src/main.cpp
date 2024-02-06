c05adcf75c58ae029e1918bc22e1b6fde6339ace
LordLizard Notebook <WarriorXKZ@gmail.com>
2024-02-06 20:43:51 +0000

    change send method

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,7 @@ private:
     int PORT;
     int sock_;
     sockaddr_in recvAddr_;
+    sockaddr_in broadcastAddr;
 
 public:
     IPv4Chat(std::string IP ,int PORT) : sock_(-1) {
@@ -27,8 +28,14 @@ public:
         recvAddr_.sin_family = AF_INET;
         recvAddr_.sin_port = htons(PORT);
         recvAddr_.sin_addr.s_addr = htonl(INADDR_ANY);
-        // recvAddr_.sin_addr.s_addr = htonl(INADDR_BROADCAST);
-        inet_pton(AF_INET, BROADCAST_IP, &recvAddr_.sin_addr);
+        // inet_pton(AF_INET, BROADCAST_IP, &recvAddr_.sin_addr);
+
+        memset(&broadcastAddr, 0, sizeof(broadcastAddr));
+
+        broadcastAddr.sin_family = AF_INET;
+        broadcastAddr.sin_port = htons(PORT);
+        recvAddr_.sin_addr.s_addr = htonl(INADDR_BROADCAST);
+        inet_pton(AF_INET, BROADCAST_IP, &broadcastAddr.sin_addr);
     }
 
     ~IPv4Chat() {
@@ -37,22 +44,22 @@ public:
         }
     }
 
-    bool initializeReceiver() {
+    bool initializeSocket() {
         sock_ = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
         if (sock_ == -1) {
             std::cerr << "Error: Could not create socket\n";
             return false;
         }
 
-        // sockaddr_in recvAddr__;
-
-        // memset(&recvAddr__, 0, sizeof(recvAddr__));
-        // recvAddr__.sin_family = AF_INET;
-        // recvAddr__.sin_port = htons(8888);
-        // recvAddr__.sin_addr.s_addr = htonl(INADDR_ANY);
-
         if (bind(sock_, (sockaddr*)&recvAddr_, sizeof(recvAddr_)) == -1) {
             std::cerr << "Error: Bind failed\n";
+            close(sock_);
+            return false;
+        }
+
+        int broadcastPermission = 1;
+        if (setsockopt(sock_, SOL_SOCKET, SO_BROADCAST, (void *)&broadcastPermission, sizeof(broadcastPermission)) == -1){
+            std::cerr << "Error: Setsock failed\n";
             close(sock_);
             return false;
         }
@@ -83,36 +90,12 @@ public:
         }
     }
 
+
     void send(const std::string& message) {
-        int sock = socket(AF_INET, SOCK_DGRAM, 0);
-        if (sock == -1) {
-            std::cerr << "Error: Could not create socket\n";
-            return;
-        }
-
-        sockaddr_in broadcastAddr;
-        memset(&broadcastAddr, 0, sizeof(broadcastAddr));
-
-        broadcastAddr.sin_family = AF_INET;
-        broadcastAddr.sin_port = htons(PORT);
-        // broadcastAddr.sin_port = htons(8888);
-
-        int broadcastPermission = 1;
-        if (setsockopt(sock_, SOL_SOCKET, SO_BROADCAST, (void *)&broadcastPermission, sizeof(broadcastPermission)) == -1){
-            std::cerr << "Error: Setsock failed\n";
-            close(sock_);
-            return;
-        }
-
-
-        inet_pton(AF_INET, BROADCAST_IP, &broadcastAddr.sin_addr);
-
-        if (sendto(sock, message.c_str(), message.length(), 0,
+        if (sendto(sock_, message.c_str(), message.length(), 0,
                    (sockaddr*)&broadcastAddr, sizeof(broadcastAddr)) == -1) {
             std::cerr << "Error: Failed to send message\n";
         }
-
-        close(sock);
     }
 };
 
@@ -130,7 +113,7 @@ void* senderThread(void* arg) {
     std::cin.ignore(); 
     while (true) {
         std::string message;
-        std::cout << "Enter message: ";
+        // std::cout << "Enter message: ";
         std::getline(std::cin, message);
         if (message.empty()) {
             std::cerr << "Error: Message cannot be empty\n";
@@ -153,7 +136,7 @@ int main(int argc, char const* argv[]) {
     }
 
     IPv4Chat chat(argv[1] , std::atoi(argv[2]));
-    if (!chat.initializeReceiver()) {
+    if (!chat.initializeSocket()) {
         return 1;
     }
 
